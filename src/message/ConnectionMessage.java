c60c50289314fe68d966d5229f74363765638edc
Amrit Rathie <amrit.rathie@gmail.com>
2023-10-27 16:59:08 -0500

    Add Message format

---
+++ src/message/ConnectionMessage.java
@@ -0,0 +1,5 @@
+package message;
+
+public class ConnectionMessage {
+    String clientName;
+}
---
+++ src/message/MessageSerializerDeserializer.java
@@ -0,0 +1,14 @@
+package message;
+
+public class MessageSerializerDeserializer {
+
+    public Message deserialize(String serializedMessage) {
+        // deserialize to message
+        return null;
+    }
+
+    public String serializeMessage(Message message) {
+        // serialize message
+        return "";
+    }
+}
